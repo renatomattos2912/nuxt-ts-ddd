2afd04b6fb3f7ffbe6e89ad23731741e194209fc
renatomattos2912 <renatomattos2912@gmail.com>
2020-03-06 14:23:59 -0300

    dotenv added

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    ['@nuxtjs/dotenv', { path: './config' }]
   ],
   /*
    ** Axios module configuration

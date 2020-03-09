162f6df49a45308bd5da2bae36c88cdbde20609f
renatomattos2912 <renatomattos2912@gmail.com>
2020-03-09 15:23:33 -0300

    remove some files

--- src/app/core/infra/BaseController.ts
+++ src/app/core/infra/BaseController.ts
@@ -1,72 +1,90 @@
-
-import * as express from 'express'
+import * as express from 'express';
 
 export abstract class BaseController {
   // or even private
-  protected req: express.Request;
-  protected res: express.Response;
+  protected req!: express.Request;
+  protected res!: express.Response;
 
-  protected abstract executeImpl (): Promise<void | any>;
+  protected abstract executeImpl(): Promise<void | any>;
 
-  public execute (req: express.Request, res: express.Response): void {
+  public execute(req: express.Request, res: express.Response): void {
     this.req = req;
     this.res = res;
 
     this.executeImpl();
   }
 
-  public static jsonResponse (res: express.Response, code: number, message: string) {
-    return res.status(code).json({ message })
+  public static jsonResponse(
+    res: express.Response,
+    code: number,
+    message: string
+  ) {
+    return res.status(code).json({ message });
   }
 
-  public ok<T> (res: express.Response, dto?: T) {
-    if (!!dto) {
+  public ok<T>(res: express.Response, dto?: T) {
+    if (dto) {
       return res.status(200).json(dto);
     } else {
       return res.sendStatus(200);
     }
   }
 
-  public created (res: express.Response) {
+  public created(res: express.Response) {
     return res.sendStatus(201);
   }
 
-  public clientError (message?: string) {
-    return BaseController.jsonResponse(this.res, 400, message ? message : 'Unauthorized');
+  public clientError(message?: string) {
+    return BaseController.jsonResponse(
+      this.res,
+      400,
+      message || 'Unauthorized'
+    );
   }
 
-  public unauthorized (message?: string) {
-    return BaseController.jsonResponse(this.res, 401, message ? message : 'Unauthorized');
+  public unauthorized(message?: string) {
+    return BaseController.jsonResponse(
+      this.res,
+      401,
+      message || 'Unauthorized'
+    );
   }
 
-  public paymentRequired (message?: string) {
-    return BaseController.jsonResponse(this.res, 402, message ? message : 'Payment required');
+  public paymentRequired(message?: string) {
+    return BaseController.jsonResponse(
+      this.res,
+      402,
+      message || 'Payment required'
+    );
   }
 
-  public forbidden (message?: string) {
-    return BaseController.jsonResponse(this.res, 403, message ? message : 'Forbidden');
+  public forbidden(message?: string) {
+    return BaseController.jsonResponse(this.res, 403, message || 'Forbidden');
   }
 
-  public notFound (message?: string) {
-    return BaseController.jsonResponse(this.res, 404, message ? message : 'Not found');
+  public notFound(message?: string) {
+    return BaseController.jsonResponse(this.res, 404, message || 'Not found');
   }
 
-  public conflict (message?: string) {
-    return BaseController.jsonResponse(this.res, 409, message ? message : 'Conflict');
+  public conflict(message?: string) {
+    return BaseController.jsonResponse(this.res, 409, message || 'Conflict');
   }
 
-  public tooMany (message?: string) {
-    return BaseController.jsonResponse(this.res, 429, message ? message : 'Too many requests');
+  public tooMany(message?: string) {
+    return BaseController.jsonResponse(
+      this.res,
+      429,
+      message || 'Too many requests'
+    );
   }
 
-  public todo () {
+  public todo() {
     return BaseController.jsonResponse(this.res, 400, 'TODO');
   }
 
-  public fail (error: Error | string) {
-    console.log(error);
+  public fail(error: Error | string) {
     return this.res.status(500).json({
       message: error.toString()
-    })
+    });
   }
 }

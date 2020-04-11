d01b8dd01c480db33b86a1a781fe8a71abe8a714
renatomattos2912 <renatomattos2912@gmail.com>
2020-04-11 17:42:53 -0300

    puppeteer working

--- puppeteer.js
+++ puppeteer.js
@@ -2,20 +2,26 @@ require('dotenv').config({ path: './config/.env' });
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    devtools: true,
-    defaultViewport: null,
-    args: ['--disable-infobars', '--start-maximized', '--disable-web-security'],
-  });
-  const pages = await browser.pages();
-  const page = pages[0];
-  const url = process.env.BASE_URL || 'http://localhost:3000';
+  // const HOST = process.env.HOST || 'localhost';
+  // const PORT = process.env.PORT || 3000;
+  const URL = process.env.BASE_URL || 'http://localhost:3000';
 
-  await page.setBypassCSP(true);
-  await page.goto(url, {
-    waitUntil: 'networkidle2',
-  });
-  // await page.pdf({ path: 'hn.pdf', format: 'A4' });
-  // await browser.close();
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      defaultViewport: null,
+      args: ['--disable-web-security'],
+    });
+    const pages = await browser.pages();
+    const page = pages[0];
+
+    await page.setBypassCSP(true);
+    await page.goto(URL, {
+      waitUntil: 'networkidle2',
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log('');
+  }
 })();
